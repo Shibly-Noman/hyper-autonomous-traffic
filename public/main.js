4cee83d47c7e1289f2cd6118b81d7132593ca7c8
Shibly Noman <shiblynoman20@gmail.com>
2020-05-29 05:30:18 +0600

    Zoom Effect Setup

--- public/main.js
+++ public/main.js
@@ -1,8 +1,26 @@
+let sf = 1;
+let mx, my;
+
 function setup(){
     createCanvas(1905, 900);
-
 }
 
 function draw(){
+    mx = mouseX;
+    my = mouseY;
     background(155);
+    translate(mx, my);
+    scale(sf);
+    translate(-mx, -my);
+    translate();
+    road.drawRoad();
+    noFill()
+    rect(0, 0, 1900, 890);
 }
+
+window.addEventListener("wheel", function(e) {
+    if (e.deltaY > 0)
+      sf *= 1.05;
+    else
+      sf *= 0.95;
+  })
--- public/road.js
+++ public/road.js
@@ -1,3 +1,25 @@
-let road = {
-    
+const road = {
+    setRoad: function(startX, startY, endX, endY){
+        if(startX === endX){
+            stroke(255);
+            strokeWeight(4)
+            line(startX, startY, endX, endY);
+            line(startX+100, startY, endX+100, endY);
+        }
+        else if(startY === endY){
+            stroke(255);
+            strokeWeight(4)
+            line(startX, startY, endX, endY);
+            line(startX, startY+100, endX, endY+100);
+            line(endX-40, endY+10, endX, endY+10);
+            line(endX-40, endY+20, endX, endY+20);
+            line(endX-40, endY+30, endX, endY+30);
+            
+        }
+    },
+    drawRoad: function(){
+        this.setRoad(100, 200, 100, 400);
+        this.setRoad(0, 100, 400, 100);
+        this.setRoad(100, 600, 600, 600);
+    } 
 }
